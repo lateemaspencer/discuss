3cd96191bc29a22fb2954516e42f7c4175737732
Lateema Spencer <spencer.lateema@gmail.com>
2024-03-11 00:14:09 -0400

    created popover form. received form data. added validation with zod. created useformstate type erros. displayed validation errors as well as general form errors.

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,24 @@
 'use server';
+import { z } from 'zod';
+import { auth } from '@/auth';
 
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
 /*
  create a new record that is going to
 // be listed out on the homepage
@@ -10,4 +29,33 @@
  to revalidate
  TODO: revalidate the homepage
  */
-export async function createTopic() {}
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+  // const name = formData.get('name');
+  // const description = formData.get('description');
+
+  // console.log(`Name: ${name} Description: ${description}`);
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this'],
+      },
+    };
+  }
+  return {
+    errors: {},
+  };
+}
