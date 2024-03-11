999a07ee50465e08a839f574de7aa9d30cf8be9e
Lateema Spencer <spencer.lateema@gmail.com>
2024-03-11 12:37:23 -0400

    connected topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,18 @@
-export default function TopicShowPage() {
-  return <div>Topic Show</div>;
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h3 className="text-2xl font-bold mb-2">{slug}</h3>
+      </div>
+      <div></div>
+    </div>
+  );
 }
