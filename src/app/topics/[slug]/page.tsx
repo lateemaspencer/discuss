e4470d62a6469613b18ed4fdd74dec1d8d67d749
Lateema Spencer <spencer.lateema@gmail.com>
2024-03-24 02:26:59 -0400

    define in parent fetch in child

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,9 +1,8 @@
 import PostCreateForm from '@/components/posts/post-create-form';
-
+import PostList from '@/components/posts/post-list';
+import { fetchPostsByTopicSlug } from '@/db/queries/posts';
 interface TopicShowPageProps {
-  params: {
-    slug: string;
-  };
+  params: { slug: string };
 }
 
 export default function TopicShowPage({ params }: TopicShowPageProps) {
@@ -12,7 +11,8 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
-        <h3 className="text-2xl font-bold mb-2">{slug}</h3>
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
       <div>
         <PostCreateForm slug={slug} />
